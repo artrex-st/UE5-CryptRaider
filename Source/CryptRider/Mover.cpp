f8c4e9e8cf15fd015f1db80894e5d97e08f3ac03
Arthur Stefanelli <artrexwow@gmail.com>
2024-02-18 17:10:15 -0300

    Dereferences

---
+++ Source/CryptRider/Mover.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Mover.h"
+
+// Sets default values for this component's properties
+UMover::UMover()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UMover::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UMover::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	AActor* TheOwner = GetOwner();
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	FString Position = TheOwner->GetActorLocation().ToCompactString();
+	UE_LOG(LogTemp,Display, TEXT("TheOwner Address is: %p and the location is: %s"),TheOwner, *Position);
+
+	// ...
+}
+
